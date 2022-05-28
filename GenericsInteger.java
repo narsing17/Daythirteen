258d94fce0112e02710b55ab023e6282603dd57b
narsing17 <narsingp14@gmail.com>
2022-05-28 14:24:19 +0530

    maximum intgernumber

---
+++ GenericsInteger.java
@@ -0,0 +1,19 @@
+package max.com;
+
+public class GenericsInteger {
+    public static Integer compareTo(Integer a, Integer b, Integer c) {
+        if (a > b && a > c) {
+            return a;
+        } else if (b > c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
+
+    public static void main(String[] args) {
+        // Welcome to Generics program.
+
+        System.out.println("Maximun number out of three is: " + compareTo(5, 7, 8));
+    }
+}
